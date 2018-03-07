d6a821d35eec5fcbdafb68c4cef634ff34af8133
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-03-07 21:31:56 +0000

    Introduce real implementation for main class.

--- src/main/scala/io/sdkman/DbCleanup.scala
+++ src/main/scala/io/sdkman/DbCleanup.scala
@@ -1,14 +1,23 @@
 package io.sdkman
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scalaj.http.Http
+import languageFeature.postfixOps
 
 class DbCleanup extends VersionsRepo with EmailConnector {
 
   def start() = {
 
-    send(Seq("http://wiremock:8080/candidates/scala/2.9.0"), smtpToEmail)
+    send(
+      Await.result(findAllVersions(), 10 seconds)
+        .filter(hasOrphanedUrl).map(_.url),
+      smtpToEmail)
 
     println("Finished working...")
   }
+
+  private def hasOrphanedUrl(version: Version): Boolean = Http(version.url).method("HEAD").asString.code != 200
 }
 
 object DbCleanup extends DbCleanup with App {
