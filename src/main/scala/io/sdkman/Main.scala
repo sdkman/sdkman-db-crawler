af6ba63da6780dd4af2fa1d8c475d87c63387d92
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-06-03 23:29:18 +0100

    Reshuffle main run method.

--- src/main/scala/io/sdkman/Main.scala
+++ src/main/scala/io/sdkman/Main.scala
@@ -16,16 +16,18 @@ class Main extends VersionsRepo
   def run(): Unit = {
     logger.info("Starting sdkman-db-cleanup...")
 
-    send(result(findAllVersions(), 10 seconds).filter { v =>
-      logger.info(s"Version: ${v.candidate}:${v.version} - ${v.url}")
-      if (v.version.endsWith("oracle"))
-        !resourceAvailable(v.url, Some(Cookie("oraclelicense", "accept-securebackup-cookie")))
-      else !resourceAvailable(v.url)
-    }, smtpToEmail)
+    val orphans = result(findAllVersions().map(vs => vs.filter(validUrl)), 20 minutes)
+    send(orphans, smtpToEmail)
 
+    Thread.sleep(10000L)
     logger.info("Stopping sdkman-db-cleanup...")
     System.exit(0)
   }
+
+  private def validUrl(v: Version): Boolean =
+    if (v.version.endsWith("oracle"))
+      !resourceAvailable(v.url, Some(Cookie("oraclelicense", "accept-securebackup-cookie")))
+    else !resourceAvailable(v.url)
 }
 
 object Main extends Main with App {
